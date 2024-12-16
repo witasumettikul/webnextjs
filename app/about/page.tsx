9bf0557bc2643fcd1510f48de41af62465d28bd5
witasumettikul <juwitawulandari2612@gmail.com>
2024-12-16 18:57:11 +0700

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <h1 className='text-2xl font-bold'>About page</h1>
+        <a href="http://localhost:3000/" className="text-blue-500 underline">Home Page</a>
+    </div>
+  )
+}
+
+export default About
---
+++ components/HelloWorld.tsx
@@ -0,0 +1,11 @@
+function HelloWorld() {
+  return (
+    <>
+        <h1>hello world</h1>
+        <p>selamat belajar nextjs</p>
+        <button>Login</button>
+    </>
+  )
+}
+
+export default HelloWorld
